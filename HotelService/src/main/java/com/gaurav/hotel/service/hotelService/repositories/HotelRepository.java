1b28d65c332e5d4d561dffb04e237e85dea2b11e
Gaurav Suman <gaurav.suman@hotmail.com>
2023-03-19 17:07:50 +0530

    reset project folder

--- HotelService/src/main/java/com/gaurav/hotel/service/hotelService/repositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gaurav.hotel.service.hotelService.repositories;
-
-import com.gaurav.hotel.service.hotelService.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
--- UserService/src/main/java/com/gaurav/user/service/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gaurav.user.service.repositories;
-
-import com.gaurav.user.service.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
