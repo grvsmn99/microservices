c227e9a6e975ffcdcd927cc5299d29d8f892e671
Gaurav Suman <gaurav.suman@hotmail.com>
2023-03-20 23:40:35 +0530

    Added circuit breaker pattern using Resilience4J

--- hotel.rating.system/UserService/src/main/java/com/gaurav/user/service/controllers/UserController.java
+++ hotel.rating.system/UserService/src/main/java/com/gaurav/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.gaurav.user.service.controllers;
 
 import com.gaurav.user.service.entities.User;
 import com.gaurav.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
     //create User
@@ -23,9 +27,19 @@ public class UserController {
 
     //single user get
     @GetMapping("/{id}")
+    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUser(@PathVariable(name="id") String userId){
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //Create fallback method for ratingHotelBreaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
+        logger.info("ratingHotelFallback Fallback is excecuted!!!",ex.getMessage());
+        User user = User.builder().about("Its a dummy user!!!")
+                .email("dummy@dummymail.com")
+                .name("Dummy").build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     //all user get
