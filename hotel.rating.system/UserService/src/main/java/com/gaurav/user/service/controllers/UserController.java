50a106a493071e02758b31edb91ae719ff55ccc8
Gaurav Suman <gaurav.suman@hotmail.com>
2023-03-20 23:56:41 +0530

    added retry machnism by Resilience4J

--- hotel.rating.system/UserService/src/main/java/com/gaurav/user/service/controllers/UserController.java
+++ hotel.rating.system/UserService/src/main/java/com/gaurav/user/service/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.gaurav.user.service.controllers;
 import com.gaurav.user.service.entities.User;
 import com.gaurav.user.service.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +27,25 @@ public class UserController {
     }
 
     //single user get
+    int retryCount;
     @GetMapping("/{id}")
-    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+//  @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+    @Retry(name="ratingHotelRetry", fallbackMethod="ratingHotelRetryFallback")
     public ResponseEntity<User> getUser(@PathVariable(name="id") String userId){
+        logger.info("Retry count: {}",retryCount);
+        retryCount++;
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //Create a method for Fallback to retry
+
+    public ResponseEntity<User> ratingHotelRetryFallback(String userId, Exception ex){
+        logger.info("ratingHotelRetryFallback Fallback is excecuted!!!",ex.getMessage());
+        User user = User.builder().about("Its a dummy user!!!")
+                .email("dummy@dummymail.com")
+                .name("Dummy").build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     //Create fallback method for ratingHotelBreaker
